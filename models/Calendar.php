28599099f65b11e9d20be52c87a5bae4ac97455a
Fabio <fabiodev3@gmail.com>
2023-11-16 22:57:29 +0100

    maximum days checker in calendar (fixed)

--- models/Calendar.php
+++ models/Calendar.php
@@ -22,18 +22,35 @@ class Calendar extends ActiveRecord{
 
     public function validateCalendar($calendarFull){
 
-        $validWeekDays = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+        $validWeekDaysAndCount = [
+            "Lunes" => 0,
+            "Martes" => 0,
+            "Miercoles" => 0,
+            "Jueves" => 0,
+            "Viernes" => 0,
+            "Sabado" => 0,
+            "Domingo" => 0
+        ];
+        $days = array_keys($validWeekDaysAndCount);
+
+        //Check if the day has more than 3 entries
+        foreach ($calendarFull as $value){
+            if(in_array($value->weekDay, $days)){
+                $lastValue = $validWeekDaysAndCount[$value->weekDay];
+                $validWeekDaysAndCount[$value->weekDay] = $lastValue += 1;
+            }
+        }
 
         if(!$this->userId || !$this->projectId || !$this->weekDay || !$this->hours || $this->userId !== $_SESSION["id"]){
             self::$alerts["error"][] = "Error al agregar al calendario, intentelo de nuevo más tarde";
         }
 
-        if(!in_array($this->weekDay, $validWeekDays)){
+        if(!in_array($this->weekDay, $days)){
             self::$alerts["error"][] = "Error al elegir el día, intentelo de nuevo más tarde";
         }
 
-        if(count($calendarFull) >= 3){
-            self::$alerts["error"][] = "No se pueden añadir más de 3 proyectos por día";
+        if($validWeekDaysAndCount[$this->weekDay] >= 3){
+            self::$alerts["error"][] = "No se pueden añadir más de 3 proyectos al mismo día";
         }
 
         return self::$alerts;
